7548df47ced4c7dcc79fcb40fc80173f95c8d765
Pranitoshyadav <pramod.y2070@gmail.com>
2026-03-12 21:02:04 +0545

    Add files via upload

---
+++ operaters1.py
@@ -0,0 +1,9 @@
+"""Write to check a number is divisible by another number."""
+
+num1 = int(input("num1 : "))
+num2 = int(input("num2 : "))
+
+if num1 % num2 == 0 :
+    print("Ther are divisable")
+else:
+    print("Ther are not divisable")
---
+++ still1.py
@@ -0,0 +1,17 @@
+"""Three cyclists are riding at the speed of 10,20,30 km/h. find the average and compare which cyclist is riding slower than the average speed?"""
+
+a = 38
+b = 36
+c = 56
+d = 40
+sum = a*d
+
+print("the sum of 40 numers is : ", sum)
+
+e = sum-((b)-(c))
+
+print("sum-((b)-(c)) is : ", e)
+
+f = e/d
+
+print(f)
